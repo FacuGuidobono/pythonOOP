22b20f933d00fd40c2d60bbc3da682ede88258b8
FacuGuidobono <facundo.guidobono@gmail.com>
2023-09-23 13:34:12 -0300

    Updated

--- Practico_7.2/Ejercicio_12/main.py
+++ Practico_7.2/Ejercicio_12/main.py
@@ -1,9 +1,9 @@
 '''
 +--------------------------------------------------------------------+
+|                        Ejercicio 12                                |
 |                                                                    |
-|                       Ejercicio 12                                 |
-| - lista que contiene los elementos de l excepto aquellos elementos |
-|    para los cuales el predicado p es verdadero.                    |
+|   lista que contiene los elementos de l excepto aquellos elementos |
+|            para los cuales el predicado p es verdadero.            |
 |                                                                    |
 +--------------------------------------------------------------------+
 
@@ -17,17 +17,27 @@ def remove_if(p: callable , l: list = None) -> list:
 
 
 
-p = lambda x: x % 2 == 0 # elimina todos los elementos de la lista que son pares
+def main():
 
-old_lista = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11]
+  #----------------------------------------------------------------
+  print_box("Se eliminaran todos los elementos de la lista que son pares [x % 2 == 0]","red")
+  p = lambda x: x % 2 == 0 # elimina todos los elementos de la lista que son pares
+  old_lista = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12]
+  printc(f"Lista original: {old_lista}\n", "blue")
+  new_lista = remove_if(p, old_lista)
+  printc(f"Lista modificada: {new_lista}\n", "green")
+#------------------------------------------------------------------
+  msg_continuar()
+  clear_console()
+#------------------------------------------------------------------
+  print_box("Se eliminaran todos los elementos de la lista que son multiplos de tres [x % 3 == 0]","yellow")
+  p = lambda x: x % 3 == 0 # elimina todos los elementos de la lista que son multiplos de 3
+  printc(f"Lista original: {old_lista}\n", "blue")
+  new_lista = remove_if(p, old_lista)
+  printc(f"Lista modificada: {new_lista}\n", "green")
+  msg_continuar()
 
-
-printc(f"Lista original: {old_lista}\n", "blue")
-new_lista = remove_if(p, old_lista)
-printc(f"Lista modificada: {new_lista}\n", "green")
-
-p = lambda y: y % 3 == 0 # elimina todos los elementos de la lista que son multiplos de 3
-
-printc(f"Lista original: {old_lista}\n", "blue")
-new_lista = remove_if(p, old_lista)
-printc(f"Lista modificada: {new_lista}\n", "green")
+if __name__ == "__main__":
+   clear_console()
+   main()
+   clear_console()
