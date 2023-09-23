e10dd74aec80ee5c3bde4b6e6e7110f7b25788a2
FacuGuidobono <facundo.guidobono@gmail.com>
2023-09-23 13:11:20 -0300

    Updated

--- Practico_7.2/Ejercicio_12/main.py
+++ Practico_7.2/Ejercicio_12/main.py
@@ -9,8 +9,25 @@
 
 
 '''
+from modulos.decorators import *
 
-
-def remove_if(p, l):
+def remove_if(p: callable , l: list = None) -> list:
 
   return list(filter(lambda x: not p(x), l))
+
+
+
+p = lambda x: x % 2 == 0 # elimina todos los elementos de la lista que son pares
+
+old_lista = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11]
+
+
+printc(f"Lista original: {old_lista}\n", "blue")
+new_lista = remove_if(p, old_lista)
+printc(f"Lista modificada: {new_lista}\n", "green")
+
+p = lambda y: y % 3 == 0 # elimina todos los elementos de la lista que son multiplos de 3
+
+printc(f"Lista original: {old_lista}\n", "blue")
+new_lista = remove_if(p, old_lista)
+printc(f"Lista modificada: {new_lista}\n", "green")
