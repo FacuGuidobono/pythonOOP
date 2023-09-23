fd5824ba631e53723bbd451fbf7a65a465e1a9b7
FacuGuidobono <facundo.guidobono@gmail.com>
2023-09-23 13:52:16 -0300

    Updated

---
+++ Practico_7.2/Ejercicio_15/main.py
@@ -0,0 +1,13 @@
+
+
+
+
+from modulos.decorators import *
+
+
+def main():
+    title = 'función lambda que reciba como parámetro dos números enteros y retorne como resultado el mayor o, en caso de igualdad, el primer argumento.'
+    print_box(f"{title}",'green')
+
+if __name__ == '__main__':
+    main()  
