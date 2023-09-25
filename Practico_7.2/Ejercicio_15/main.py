012471b87b28138a6a8aeb875918de15b665e94c
FacuGuidobono <facundo.guidobono@gmail.com>
2023-09-25 09:34:57 -0300

    Updated

--- Practico_7.2/Ejercicio_15/main.py
+++ Practico_7.2/Ejercicio_15/main.py
@@ -1,13 +1,32 @@
+'''
++--------------------------------------------------------------------------------+
+|                                Ejercicio 15                                    |
+|                                                                                |
+|        función lambda que reciba como parámetro dos números enteros            |
+| y retorne como resultado el mayor o, en caso de igualdad, el primer argumento. |
+|                                                                                |
++--------------------------------------------------------------------------------+
 
 
-
-
+'''
 from modulos.decorators import *
 
 
 def main():
-    title = 'función lambda que reciba como parámetro dos números enteros y retorne como resultado el mayor o, en caso de igualdad, el primer argumento.'
+    
+    title = 'función lambda que recibe como parámetro dos números enteros y retorne como resultado el mayor o, en caso de igualdad, el primer argumento.'
     print_box(f"{title}",'green')
+    f = lambda x,y: y if y > x else x
+    try: 
+        x = int(input('Ingrese un numero entero: '))
+        y = int(input('Ingrese otro numero entero: '))
+    
+    except ValueError:
+        msg_error()
+        return
+    
+    printc(f'El mayor es: {f(x,y)}','green')
+    msg_continuar()
 
 if __name__ == '__main__':
     main()  
---
+++ Practico_7.2/Ejercicio_17/main.py
@@ -0,0 +1,27 @@
+'''
++--------------------------------------------------------------------------------+
+|                                Ejercicio 17                                    |
+|                                                                                |
+|                              funciones lambda                                  |
+|                                                                                |
+|                                                                                |
++--------------------------------------------------------------------------------+
+
+
+'''
+from modulos.decorators import *
+
+
+title = ['Calcular el doble de un número', 
+         'Determinar si un número es impar', 
+         'Dados dos strings retornar el de mayor longitud',
+         'Dada una dupla retornar otra cuya primera componente sea el doble de la primer componente de la dupla de entrada y el segundo ítem sea el triple del segundo ítem de la dupla de entrada',
+         'Determinar si un número es mayor que 0',
+         'Determinar si un número está dentro de un rango determinado',
+         'Determinar si un punto está dentro de una circunsferencia',
+         'Calcular el área de un triángulo',
+         'Calcular el área de un cuadrado',
+         'Ordenar de forma ascendente o descendente una lista de números enteros',
+         ]
+
+menu_principal(title)
