145b63f95b8c864c279b26b39bdbf81daa2d5c27
FacuGuidobono <facundo.guidobono@gmail.com>
2023-09-28 18:24:08 -0300

    Final

--- Practico_7.3/Ejercicio_14/main.py
+++ Practico_7.3/Ejercicio_14/main.py
@@ -12,14 +12,14 @@
 
 
 from modulos.decorators import *
-
+#------------------------------------------------------------------
 def generador_multiplos(num):
     n = 1
     while True:
         yield n * num
         n += 1
         
-        
+#------------------------------------------------------------------       
 def main():
     clear_console()
     print_box('         GENERADOR DE MULTIPLOS        ','magenta')
@@ -29,19 +29,16 @@ def main():
     generador = generador_multiplos(num)
     lista = []
     
-    for divisor in generador:
-       
-        printc('-'*100,'green',style='bold')
-        printc(f'Multiplo/s: {divisor}','yellow')
-        printc('-'*100,'green',style='bold')
-        print('\n')
+   
+    for _ in range(cantidad):
+        multiplo = next(generador)
+        lista.append(multiplo)
+    printc('-'*100,'green',style='bold')
+    printc(f'Multiplo/s: {lista}','yellow')
+    printc('-'*100,'green',style='bold')
+    print('\n')
             
-        
-        
-        
-                
-
-
+#------------------------------------------------------------------        
 
 if __name__ == '__main__':
     main()  
--- Practico_7.3/Ejercicio_16/main.py
+++ Practico_7.3/Ejercicio_16/main.py
@@ -14,26 +14,35 @@
 from modulos.decorators import *
 
 def generador_divisores(num):
-    for i in range(1, num + 1):
+    i = 1
+
+    while True:
         if num % i == 0:
-            yield i
-        
+            yield i   
+        i += 1
+
+    
         
 def main():
     clear_console()
     print_box('         GENERADOR DE DIVISORES        ','magenta')
     
-    num = validacion_un_numero('Ingrese un numero para obtener sus diviores: ', int)
+    num = validacion_un_numero('Ingrese un numero para obtener sus divisores: ', int)
     
     generador = generador_divisores(num)
     
-    
-    for divisor in generador:
-       
-        printc('-'*100,'green',style='bold')
-        printc(f'Multiplo/s: {divisor}','yellow')
-        printc('-'*100,'green',style='bold')
-        print('\n')
+    lista = []
+
+    for _ in range(num):
+        divisor = next(generador)
+        lista.append(divisor)
+        if divisor == num:
+            break
+
+    printc('-'*100,'green',style='bold')
+    printc(f'Divisor/es: {lista}','yellow')
+    printc('-'*100,'green',style='bold')
+    print('\n')
                 
         
         
